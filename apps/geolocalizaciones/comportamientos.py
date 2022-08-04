d2238407cf6597199d00ed4ae244f307cbbee4da
ceragos <jesus.a.castaneda.v@gmail.com>
2022-08-04 00:17:10 -0500

    Se crea comportamiento geolocalizable

---
+++ apps/geolocalizaciones/comportamientos.py
@@ -0,0 +1,35 @@
+from pyexpat import model
+from django.db import models
+from smart_selects.db_fields import ChainedForeignKey
+
+from apps.geolocalizaciones.models import Pais, Estado, Ciudad
+
+class Geolocalizable(models.Model):
+    
+    pais = models.ForeignKey(
+        Pais,
+        verbose_name='País',
+        related_name='%(class)ss_pais',
+        on_delete=models.PROTECT
+    )
+    estado = ChainedForeignKey(
+        Estado,
+        chained_field="pais",
+        chained_model_field="pais",
+        show_all=False,
+        auto_choose=True,
+        sort=True,
+        related_name='%(class)ss_estado'
+    )
+    ciudad = ChainedForeignKey(
+        Ciudad,
+        chained_field="estado",
+        chained_model_field="estado",
+        show_all=False,
+        auto_choose=True,
+        sort=True,
+        related_name='%(class)ss_ciudad'
+    )
+
+    class Meta:
+        abstract = True
